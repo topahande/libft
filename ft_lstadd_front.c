1a211b525bd0c6a8d7df4436ac5ef057c65ee8ab
topahande <ladytastingdata@gmail.com>
2024-04-28 16:03:24 +0300

    Add ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: htopa <htopa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 17:49:17 by htopa             #+#    #+#             */
-/*   Updated: 2024/04/27 17:49:19 by htopa            ###   ########.fr       */
+/*   Updated: 2024/04/28 13:44:13 by htopa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	if (lst == NULL || new == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
