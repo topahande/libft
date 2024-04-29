fcc0001ad2f42fddf08cb35ed5f576a338dc8fca
topahande <ladytastingdata@gmail.com>
2024-04-29 15:57:04 +0300

    Fix ft_memset.c
    Previous version returned wrong pointer as the pointer had moved forward through the string and it had not been moved back to the beginning of the string.

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: htopa <htopa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:58:08 by htopa             #+#    #+#             */
-/*   Updated: 2024/04/22 17:03:38 by htopa            ###   ########.fr       */
+/*   Updated: 2024/04/29 15:52:06 by htopa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*a;
+	size_t			i;
 
 	a = (unsigned char *)b;
-	while (len > 0)
+	i = 0;
+	while (i < len)
 	{
-		*a = c;
-		a++;
-		len--;
+		a[i] = c;
+		i++;
 	}
 	b = a;
 	return (b);
