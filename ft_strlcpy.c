9d6a608658c439d7d8f903392c09c2a33e447df6
topahande <ladytastingdata@gmail.com>
2024-04-29 14:13:22 +0300

    Fix ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -18,8 +18,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	srcsize = ft_strlen(src);
+	if (dstsize == 0)
+		return (srcsize);
 	i = 0;
-	while (i < dstsize -1 || i < srcsize)
+	while (i < dstsize -1 && i < srcsize)
 	{
 		dst[i] = src[i];
 		i++;
