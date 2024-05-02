844b87160b0c174144f7151b31e5ebab6f06cedb
topahande <ladytastingdata@gmail.com>
2024-05-02 16:09:44 +0300

    Fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,18 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (*s1 != '\0' && *s2 != '\0' && i < n)
-	{
-		if (*s1 != *s2)
-		{
-			break ;
-		}
-		s1++;
-		s2++;
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1 && s1[i] == s2[i])
 		i++;
-	}
-	if (i == n)
-		return (0);
-	else
-		return (*s1 - *s2);
+	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
