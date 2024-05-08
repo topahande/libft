95030c8a73b71bcebec27f55b8524d0b020193d1
topahande <ladytastingdata@gmail.com>
2024-05-08 13:42:51 +0300

    Add ft_itoa.c

--- Makefile
+++ Makefile
@@ -38,6 +38,8 @@ MY_SOURCES = ft_isalpha.c \
 			ft_substr.c \
 			ft_strjoin.c \
 			ft_strtrim.c \
+			ft_split.c \
+			ft_itoa.c \
 			ft_putchar_fd.c \
 			ft_putstr_fd.c \
 			ft_putendl_fd.c \
--- libft.h
+++ libft.h
@@ -49,6 +49,8 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
